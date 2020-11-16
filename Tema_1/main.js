b3f6cc7c210de1a72140a8ac3c2fc051d7601ec1
Ardelean Marcus <ardeleanmarcus@icloud.com>
2020-11-17 00:45:12 +0200

    Tema 1

---
+++ Tema_1/main.js
@@ -0,0 +1,13 @@
+var telefon = {
+    producator:"Apple", 
+    model:"11 Pro Max", 
+    stocare:"256 GB"
+};
+
+
+function mesaj(nume){
+    console.log("Buna, numele meu este " + nume);
+}
+
+mesaj("Marcus");
+console.log(telefon);
---
+++ main.js
@@ -0,0 +1,19 @@
+var name = "Marcus"; //string
+var ammount = 23; //numeric
+var knowsJavascript = true; //boolean
+var fruits = ["apple", "lemon", "orange"]; //array
+
+
+function add(){
+    return 2 + 3;
+}
+
+
+//tipul obiect
+var person = {
+    height: 2,
+    canWalk: function(){
+        console.log("Yes, I can walk");
+    },
+    name: "Julia"
+}
